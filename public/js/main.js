4efc4318c0de6e0381735ad7a4f9102797c82b01
Cristianc72004 <benalcazarc7@gmail.com>
2024-05-29 18:13:49 -0500

    Final

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,13 @@ document.getElementById('calcularBtn').addEventListener('click', function() {
 
     var resultado = cantidad * (porcentaje / 100);
 
+    // Verificar si el resultado tiene parte decimal
+    if (resultado % 1 !== 0) {
+        resultado = resultado.toFixed(2);
+    } else {
+        resultado = resultado.toFixed(0); // Convertir a entero si no tiene parte decimal
+    }
+
     document.getElementById('resultadoValor').textContent = resultado;
 });
 
